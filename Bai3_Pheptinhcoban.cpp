e53c4b2c0fc0ad9f87ab8f3e330d6678fd1be307
NguyenPhuThanh29082008 <nguyenphuthanhpch298@gmail.com>
2024-11-11 09:34:41 +0700

    Phep Tinh Tong Hieu Tich Thuong Co Ban

--- Bai3_Pheptinhcoban.cpp
+++ Bai3_Pheptinhcoban.cpp
@@ -1,6 +1,20 @@
 
 #include <iostream>
 using namespace std;
+
+double tinhtong(double a, double b) {
+    return a + b;
+}
+double tinhhieu(double a, double b) {
+    return a - b;
+}
+double tinhtich(double a, double b) {
+    return a * b;
+}
+double tinhthuong(double a, double b) {
+    return round(a / b);
+}
+
 int main()
 {
     cout << "CHUONG TRINH TONG HIEU TICH THUONG" << endl;
@@ -14,17 +28,17 @@ int main()
     thuong = a / b;
     tonghieutichthuong = a + b - a * b / a;
 
-    cout << "Cong Thuc Phep Tong : " << a << "+" << b << "=" << tong << endl;
-    cout << "Cong Thuc Phep Hieu : " << a << "-" << b << "=" << hieu << endl;
-    cout << "Cong Thuc Phep Nhan : " << a << "*" << b << "=" << tich << endl;
-    cout << "Cong Thuc Phep Thuong : " << a << ":" << b << "=" << thuong << endl;
+    cout << "Cong Thuc Phep Tong Binh Thuong: " << a << "+" << b << "=" << tong << endl;
+    cout << "Cong Thuc Phep Tong Binh Duong : " << a << "+" << b << "=" << tinhtong(a,b) << endl;
+    cout << "Cong Thuc Phep Hieu Binh Thuong : " << a << "-" << b << "=" << hieu << endl;
+    cout << "Cong Thuc Phep Hieu Dang Cap : " << a << "-" << b << "=" << tinhhieu(a,b) << endl;
+    cout << "Cong Thuc Phep Nhan Binh Thuong : " << a << "*" << b << "=" << tich << endl;
+    cout << "Cong Thuc Phep Nhan PRO : " << a << "*" << b << "=" << tinhtich(a,b) << endl;
+    cout << "Cong Thuc Phep Thuong Binh Thuong : " << a << ":" << b << "=" << thuong << endl;
+    cout << "Cong Thuc Phep Thuong VIP : " << a << ":" << b << "=" << tinhthuong(a,b) << endl;
     cout << "Cong Thuc Tat ca phep : " << a << "+" << b << "-" << a << "*" << b << ":" << a << "=" << tonghieutichthuong << endl;
 
-
-
-
-
-
+    
 
     return 0;
 }
